5fa65ed8ea45b958f627854f6bc8ad3a91ee7d1b
Steven Frew <steven.fullhouse@gmail.com>
2021-11-11 15:38:24 +0000

    Added comment to Mid function.

--- aspidiftra/AspidiftraUtil.cs
+++ aspidiftra/AspidiftraUtil.cs
@@ -33,6 +33,16 @@ namespace Aspidiftra
 			sourcePdf.Save(outputPath);
 		}
 
+		/// <summary>
+		///   Returns the middle N elements from a collection.
+		/// </summary>
+		/// <typeparam name="T">The generic type.</typeparam>
+		/// <param name="collection">The collection.</param>
+		/// <param name="amount">The number of elements to return.</param>
+		/// <returns>
+		///   The elements from the middle of the collection, or as near
+		///   as it can get if an odd number of elements need to be removed.
+		/// </returns>
 		public static IEnumerable<T> Mid<T>(this IEnumerable<T> collection, int amount)
 		{
 			var asList = collection.ToList();
@@ -42,7 +52,7 @@ namespace Aspidiftra
 					$"Cannot take the middle {amount} elements from a collection that only has {collectionSize} elements in it.");
 			var tooMany = collectionSize - amount;
 			var amountToRemoveEachSide = tooMany / 2;
-			return asList.GetRange(amountToRemoveEachSide, collectionSize - tooMany);
+			return asList.GetRange(amountToRemoveEachSide, amount);
 		}
 	}
 }
